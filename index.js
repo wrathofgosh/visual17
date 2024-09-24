752c4dd257e84c7ce1aa6d950e6b06ac54c81d38
wrathofgosh <davidchenxd@gmail.com>
2024-09-23 20:16:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    alert(window.location.pathname);
+    //alert(window.location.pathname);
+    var htmlBody = document.getElementById('');
+    var hash = window.location.pathname.split('/visual17/')[1];
+    if (hash.length == 32) {
+        alert("valid hash");
+    } else {
+        alert("bad hash");
+    }
 });
