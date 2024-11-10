5615f13d92d5a3e33fc9326b8fe5c2e23cb1ad2f
wrathofgosh <182560258+wrathofgosh@users.noreply.github.com>
2024-11-09 20:24:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
     var htmlBody = document.getElementById('');
     var hash = window.location.pathname.split('/visual17/')[1];
     if (hash.length == 32) {
-        fetch('https://www.17lands.com/card_tiers/data/' + hash)
+        fetch("https://www.17lands.com/card_tiers/data/" + hash)
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                 }
-                return response.json();  // Assuming the response is JSON
-            })
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+             })
             .then(data => {
                 console.log(data);
             })
-            .catch(error => console.error('There has been a problem with your fetch operation:', error));
+            .catch(error => {
+                console.error('Error:', error);
+            });
     } else {
         alert("bad hash");
     }
